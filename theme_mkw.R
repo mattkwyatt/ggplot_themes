e35a94bd3a8dc83a7cd0ba157f5791a39ef1fe1a
mattkwyatt <83123989+mattkwyatt@users.noreply.github.com>
2022-04-03 12:02:21 +0100

    Initial commit

---
+++ theme_mkw.R
@@ -0,0 +1,23 @@
+theme_mkw <- function() {
+  
+  theme(
+    # add border
+    panel.border = element_rect(colour = "black", fill = NA),
+    
+    # color background
+    panel.background = element_rect(fill = "white"),
+    
+    # modify text, axis and colour
+    text = element_text(colour = "black", family = "Arial"),
+    axis.title = element_text(colour = "black", family = "Arial"),
+    axis.ticks = element_line(colour = "black"),
+    
+    # legend at the top
+    legend.position = "top",
+    legend.background = element_rect(fill = "white"),
+    legend.key = element_rect(fill = "white"),
+    
+    # modify facet background
+    strip.background = element_rect(colour = "black", fill = "grey85", size = 1)
+  )
+}
